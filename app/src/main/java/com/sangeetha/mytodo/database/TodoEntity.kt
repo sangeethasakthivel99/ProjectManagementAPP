cd09e55103757d943bc551c64c2d7eaf0836fa33
Sangeetha <sakthivelsangeetha99@gmail.com>
2021-01-02 21:05:49 +0530

    Add Todo table in Room Database

--- app/src/main/java/com/sangeetha/mytodo/database/TodoEntity.kt
+++ app/src/main/java/com/sangeetha/mytodo/database/TodoEntity.kt
@@ -1,31 +1,41 @@
 package com.sangeetha.mytodo.database
 
+import android.os.Parcelable
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.Relation
+import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
 
 @Entity
+@Parcelize
 data class TodoEntity(
     @PrimaryKey(autoGenerate = true)
     var todoId: Long,
     var folderId: Long? = null,
     var title: String,
     var description: String? = null,
-    var createdOn: String? = null,
+    var createdOn: Long = System.currentTimeMillis(),
     var isCompleted: Boolean,
-    var duedate: String? = null
-)
+    var duedate: String? = null,
+    var isImportant: Boolean = false
+): Parcelable {
+    val getCreatedDateInFormat: String
+        get() = DateFormat.getDateTimeInstance().format(createdOn)
+}
 
 @Entity
+@Parcelize
 data class TodoFolderEntity(
     @PrimaryKey(autoGenerate = true)
     var folderId: Long,
     var folderName: String? = null,
     var folderImage: String? = null,
-    var createdOn: String? = null
-)
+    var createdOn:  Long = System.currentTimeMillis()
+): Parcelable
 
+@Parcelize
 data class FolderWithToDos(
     @Embedded
     val folder: TodoFolderEntity,
@@ -35,4 +45,4 @@ data class FolderWithToDos(
         entityColumn = "folderId"
     )
     val toDos: List<TodoEntity>
-)
+): Parcelable
