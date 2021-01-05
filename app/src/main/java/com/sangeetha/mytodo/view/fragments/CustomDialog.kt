4499efb74ea4de13e437f81fb93173d581f017a5
Sangeetha <sakthivelsangeetha99@gmail.com>
2021-01-05 23:00:45 +0530

    Add Custom Dialog

---
+++ app/src/main/java/com/sangeetha/mytodo/view/fragments/CustomDialog.kt
@@ -0,0 +1,7 @@
+package com.sangeetha.mytodo.view.fragments
+
+import androidx.fragment.app.DialogFragment
+import com.sangeetha.mytodo.R
+
+class CustomDialog: DialogFragment(R.layout.layout_custom_dialog) {
+}
