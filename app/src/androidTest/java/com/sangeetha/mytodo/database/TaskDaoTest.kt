a3270da19f693a7cf98ba9122b36d7aef704b34a
Sangeetha <sakthivelsangeetha99@gmail.com>
2021-01-07 22:49:16 +0530

    Add Room testing library

---
+++ app/src/androidTest/java/com/sangeetha/mytodo/database/TaskDaoTest.kt
@@ -0,0 +1,31 @@
+package com.sangeetha.mytodo.database
+
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import junit.framework.TestCase
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+
+class TaskDaoTest {
+
+//        @get:Rule
+//        val instantTaskExecutorRule = InstantTaskExecutorRule()
+//        private lateinit var _db: TestpressDatabase
+//        val db: TestpressDatabase
+//            get() = _db
+//
+//        @Before
+//        fun initDb() {
+//            _db = Room.inMemoryDatabaseBuilder(
+//                ApplicationProvider.getApplicationContext(),
+//                TestpressDatabase::class.java
+//            ).build()
+//        }
+//
+//        @After
+//        fun closeDb() {
+//            _db.close()
+//        }
+//    }
+}
